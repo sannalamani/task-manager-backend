c997f4305a9b7eb9a1c74df8866d831f3e822137
sannalamani <s.manik@outlook.fr>
2024-10-15 02:49:07 +0530

    Initiql commit

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
